fa41ab10d0eb0b7f75b03e16e4f5d3fadb5dd342
bufudash <54249873+bufudash@users.noreply.github.com>
2020-07-16 22:35:30 -0500

    HiClass.php done

--- hiClass.php
+++ hiClass.php
@@ -36,7 +36,7 @@ require_once('config.php');
              }
 
 
-             if ($type != 0) {
+             if ($type == 0) {
                  $message = "Your hadn't subscribe to our product, please click here to view our plans.";
                  $html = <<<HTML
                  <a href="pricing.html">Pricing</a>
@@ -46,7 +46,7 @@ require_once('config.php');
                 <a href="pricing.html">here</a>
                 HTML;
                 $message = "You ran out of class this week! Please come back later or click $html to buy single sessions.";
-             } elseif ($type == 0 && $count == 0) {
+             } elseif ($type != 0 && $count > 0) {
                 $message = <<<HTML
                 <iframe src='https://outlook.office365.com/owa/calendar/HiClassSchedulePage@hilink.llc/bookings/' width='100%' height='100%' scrolling='yes' style='border:0'></iframe>
                 HTML;
